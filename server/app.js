e3f9cf6f0e178e97ba57de568a4865b52f0c5f26
Pedro Vallejo <p.vallejo@uniandes.edu.co>
2023-01-31 10:42:37 -0500

    Express
    Server is able to run by using Express

---
+++ server/app.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const Servidor = require('./models/servidor');
+
+const servidor = new Servidor();
+
+servidor.listen();
---
+++ server/models/servidor.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+class Servidor {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Middlewares
+
+        // Rutas de la aplicacion
+        this.routes();
+    }
+
+    middlewares(){
+        
+    }
+
+    routes() {
+        this.app.get('/', (req, res) => {
+            res.send('Hello World')
+          })
+    }
+
+    listen() {
+        this.app.listen(this.PORT, () => {
+            console.log('El servidor se encuentra corriendo en el puerto:', process.env.PORT)
+        })
+    }
+}
+
+module.exports = Servidor;
